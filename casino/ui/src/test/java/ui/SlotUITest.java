66fb08c80850444cb18fc07874b8b14168797b55
Sebastian Sole <sebastrs@stud.ntnu.no>
2021-10-14 15:33:31 +0200

    Added UI tests for slots
    Added UI tests for slots UI. 91% completion.

--- casino/ui/src/test/java/ui/SlotUITest.java
+++ casino/ui/src/test/java/ui/SlotUITest.java
@@ -3,9 +3,14 @@ package ui;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
 import saveHandler.UserSaveHandler;
 import ui.RouletteController;
@@ -14,7 +19,61 @@ import user.User;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class SlotUITest {
+public class SlotUITest extends ApplicationTest{
+    private SlotsController slotsController;
+    private User user = new User("slotsTest", 100);
 
+    @Override
+    public void start(final Stage stage) throws Exception{
+        final FXMLLoader loader = new FXMLLoader(getClass().getResource("SlotsTest.fxml"));
+        final Parent root = loader.load();
+        this.slotsController = loader.getController();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+
+    @Test
+    public void betAndSpin() {
+        var prevAvgPayout = slotsController.getAvgPayout().getText();
+        var prevBalanceNum = slotsController.getBalanceNum().getText();
+        var prevNetGainNum = slotsController.getNetGainNum().getText();
+        var prevCurrentBetNum = slotsController.getCurrentBetNum().getText();
+        var prevPayoutNum = slotsController.getPayoutNum().getText();
+        var prevComboSlot = slotsController.getComboSlot().getText();
+        var prevSpinsCounter = slotsController.getSpinsCounter().getText();
+
+        clickOn("#betField").write("10");
+        clickOn("#spinButton");
+        for (HBox box : slotsController.getHboxesList()){
+            assertTrue(box.getChildren().size()!=0);
+        }
+        sleep(4000);
+        assertEquals("", slotsController.getBetField().getText());
+        assertNotEquals(prevAvgPayout, slotsController.getAvgPayout().getText());
+        assertNotEquals(prevBalanceNum, slotsController.getBalanceNum().getText());
+        assertNotEquals(prevComboSlot, slotsController.getComboSlot().getText());
+        assertNotEquals(prevCurrentBetNum, slotsController.getCurrentBetNum().getText());
+        assertNotEquals(prevNetGainNum, slotsController.getNetGainNum().getText());
+        assertNotEquals(prevSpinsCounter, slotsController.getSpinsCounter().getText());
+        if (Double.parseDouble(slotsController.getPayoutNum().getText()) !=0.0 ){
+            assertNotEquals(prevPayoutNum, slotsController.getPayoutNum().getText());
+        }
+    }
+
+    @Test
+    public void testKeepButton(){
+        clickOn("#betField").write("10");
+        clickOn("#keepBetButton");
+        clickOn("#spinButton");
+        sleep(4000);
+        assertEquals("10", slotsController.getBetField().getText());
+    }
+
+    @Test
+    public void backToLobby(){
+        clickOn("#fileButton");
+        clickOn("#lobby");
+    }
 
 }
