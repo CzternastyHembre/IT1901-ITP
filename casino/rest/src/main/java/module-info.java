68cdda5443783764b7676642ce3786c8efc225f7
Erlend <sorknerl@gmail.com>
2021-11-10 10:21:50 +0100

    chore: Added essential files
    Added pom.xml files and module-info files that was not added in the initial commit. Rest module will now work when cloned.
    
    Issue: #31

---
+++ casino/rest/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module rest {
+    requires spring.web;
+    requires core;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires com.google.gson;
+    requires java.net.http;
+
+}
