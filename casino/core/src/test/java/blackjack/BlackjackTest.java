be2b0bb65caedcfc0b5b43ae0f3a8065294da584
Sebastian Sole <sebastrs@stud.ntnu.no>
2021-11-24 23:56:45 +0100

    test: Beginning of test correction in BlackjackTest
    Began rewriting tests for BlackjackTest
    
    Issue: #67

--- casino/core/src/test/java/blackjack/BlackjackTest.java
+++ casino/core/src/test/java/blackjack/BlackjackTest.java
@@ -137,96 +137,186 @@ class BlackjackTest {
         Assertions.assertEquals(blackjack.getDealingDeck().getDeck().size(),48);
         Assertions.assertTrue(blackjack.getPlayersHand1().isActive());
         Assertions.assertFalse(blackjack.getPlayersHand2().isActive());
-        if (blackjack.isInstantBlackjack()){
-            if (blackjack.getDealersHand().getSumOfDeck() == 21){
-                assertEquals(blackjack.getPayout(), blackjack.getBet());
-            }
-            else{
-                assertEquals(blackjack.getPayout(), blackjack.getBet()*1.5);
-            }
-        }
-        else {
-            Assertions.assertEquals(blackjack.getPayout(), 0);
-        }
-        Assertions.assertEquals(blackjack.getDealersHand().getDeck().size(),2);
-        Assertions.assertEquals(blackjack.getTargetHand(),blackjack.getPlayersHand1());
+
+
+//        if (blackjack.isInstantBlackjack()){
+//            if (blackjack.getDealersHand().getSumOfDeck() == 21){
+//                assertEquals(blackjack.getPayout(), blackjack.getBet());
+//            }
+//            else{
+//                assertEquals(blackjack.getPayout(), blackjack.getBet()*1.5);
+//            }
+//        }
+//        else {
+//            Assertions.assertEquals(blackjack.getPayout(), 0);
+//        }
+//        Assertions.assertEquals(blackjack.getDealersHand().getDeck().size(),2);
+//        Assertions.assertEquals(blackjack.getTargetHand(),blackjack.getPlayersHand1());
     }
 
     @Test
-    void hit() {
+    void naturalBlackjackWin(){
+        naturalBlackjackWinDeck();
+        blackjack.setDealingDeck(testDeck);
         blackjack.startGame(10);
-        int handSize = blackjack.getTargetHand().getDeck().size();
-        Card cardToAdd = blackjack.getDealingDeck().getDeck().get(0);
-        blackjack.hit();
-        assertFalse(blackjack.isCanSplit());
-        Assertions.assertEquals(blackjack.getTargetHand().getDeck().size(),handSize+1);
-        Assertions.assertEquals(blackjack.getTargetHand().getLastCard(), cardToAdd);
-        if (blackjack.getTargetHand().getSumOfDeck() > 21){
-            Assertions.assertFalse(blackjack.getTargetHand().isActive());
-            blackjack.stand();
-            Assertions.assertThrows(IllegalArgumentException.class, () -> {
-                blackjack.hit();
-            });
-        }
-        else {
-            Assertions.assertTrue(blackjack.getTargetHand().isActive());
-        }
-
+        assertTrue(blackjack.isInstantBlackjack());
+        assertFalse(blackjack.getDealersHand().isActive());
+        assertEquals(blackjack.getPayout(), blackjack.getBet()*1.5);
+        assertEquals(blackjack.getUser().getBalance(), 1005);
     }
 
     @Test
-    void stand() {
+    void naturalBlackjackTie(){
+        naturalBlackjackTieDeck();
+        blackjack.setDealingDeck(testDeck);
         blackjack.startGame(10);
-        blackjack.stand();
-        Assertions.assertFalse(blackjack.getTargetHand().isActive());
+        assertTrue(blackjack.isInstantBlackjack());
+        assertFalse(blackjack.getDealersHand().isActive());
+        assertEquals(blackjack.getPayout(), blackjack.getBet());
+        assertEquals(blackjack.getUser().getBalance(),1000);
     }
 
     @Test
-    void split() {
+    void splittableDeck(){
+        setSplittableDeck();
+        blackjack.setDealingDeck(testDeck);
         blackjack.startGame(10);
-
-        Hand splittableHand = new Hand();
-        splittableHand.getDeck().add(new Card(5,'C'));
-        splittableHand.getDeck().add(new Card(5,'H'));
-        Hand insplittableHand = new Hand();
-        insplittableHand.getDeck().add(new Card(5,'C'));
-        insplittableHand.getDeck().add(new Card(6,'H'));
-        double userBalance = blackjack.getUser().getBalance();
-
-        blackjack.setPlayersHand1(insplittableHand);
-        blackjack.split();
-        Assertions.assertEquals(userBalance, user.getBalance()); // If this is true, the rest of the method hasn't been called
-        blackjack.setPlayersHand1(splittableHand);
-        blackjack.split();
-        assertEquals(blackjack.getUser().getBalance(), userBalance - blackjack.getBet());
-        assertEquals(blackjack.getPlayersHand1().getDeck().size(), 1);
-        assertEquals(blackjack.getPlayersHand2().getDeck().size(),1);
-        assertTrue(blackjack.getPlayersHand2().isActive());
-        assertEquals(blackjack.getTargetHand(), blackjack.getTargetHand());
-        assertTrue(blackjack.isHasSplit());
-
-    }
-
-    @Test
-    void toggleTargetHand() {
-        blackjack.startGame(10);
-        blackjack.toggleTargetHand();
-        assertEquals(blackjack.getTargetHand(), blackjack.getPlayersHand2());
-        blackjack.toggleTargetHand();
+        assertTrue(blackjack.canSplit());
         assertEquals(blackjack.getTargetHand(), blackjack.getPlayersHand1());
     }
 
     @Test
-    void dealerPlay() {
+    void hitOverLimit(){
+        hitOverLimitDeck();
+        blackjack.setDealingDeck(testDeck);
         blackjack.startGame(10);
-        blackjack.dealerPlay();
-        Assertions.assertTrue(blackjack.getDealersHand().getSumOfDeck() >= 17);
-        Assertions.assertFalse(blackjack.getDealersHand().isActive());
-        if (blackjack.getDealersHand().getSumOfDeck() == 21){
-            assertEquals(0, blackjack.getPayout());
-        }
-        if (blackjack.getPlayersHand2().getSumOfDeck()!=0){
-            assertTrue(blackjack.getPayout()-blackjack.getBet() != blackjack.getPayout());
-        }
+        blackjack.hit();
+        assertFalse(blackjack.getTargetHand().isActive());
+        assertFalse(blackjack.canSplit());
+        assertEquals(3, blackjack.getTargetHand().getDeck().size());
     }
+
+//    @Test
+//    void hitUnderLimit(){
+//        hitUnderLimitDeck();
+//        blackjack.setDealingDeck(testDeck);
+//
+//    }
+
+
+
+
+
+
+
+
+    //todo; test hit on inactive deck
+
+
+
+
+//    @Test
+//    void hit() {
+//        blackjack.startGame(10);
+//        int handSize = blackjack.getTargetHand().getDeck().size();
+//        Card cardToAdd = blackjack.getDealingDeck().getDeck().get(0);
+//        blackjack.hit();
+//        assertFalse(blackjack.isCanSplit());
+//        Assertions.assertEquals(blackjack.getTargetHand().getDeck().size(),handSize+1);
+//        Assertions.assertEquals(blackjack.getTargetHand().getLastCard(), cardToAdd);
+//        if (blackjack.getTargetHand().getSumOfDeck() > 21){
+//            Assertions.assertFalse(blackjack.getTargetHand().isActive());
+//            blackjack.stand();
+//            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+//                blackjack.hit();
+//            });
+//        }
+//        else {
+//            Assertions.assertTrue(blackjack.getTargetHand().isActive());
+//        }
+//
+//    }
+//
+//    @Test
+//    void stand() {
+//        blackjack.startGame(10);
+//        blackjack.stand();
+//        Assertions.assertFalse(blackjack.getTargetHand().isActive());
+//    }
+//
+//    @Test
+//    void split() {
+//        blackjack.startGame(10);
+//
+//        Hand splittableHand = new Hand();
+//        splittableHand.getDeck().add(new Card(5,'C'));
+//        splittableHand.getDeck().add(new Card(5,'H'));
+//        Hand insplittableHand = new Hand();
+//        insplittableHand.getDeck().add(new Card(5,'C'));
+//        insplittableHand.getDeck().add(new Card(6,'H'));
+//        double userBalance = blackjack.getUser().getBalance();
+//
+//        blackjack.setPlayersHand1(insplittableHand);
+//        blackjack.split();
+//        Assertions.assertEquals(userBalance, user.getBalance()); // If this is true, the rest of the method hasn't been called
+//        blackjack.setPlayersHand1(splittableHand);
+//        blackjack.split();
+//        assertEquals(blackjack.getUser().getBalance(), userBalance - blackjack.getBet());
+//        assertEquals(blackjack.getPlayersHand1().getDeck().size(), 1);
+//        assertEquals(blackjack.getPlayersHand2().getDeck().size(),1);
+//        assertTrue(blackjack.getPlayersHand2().isActive());
+//        assertEquals(blackjack.getTargetHand(), blackjack.getTargetHand());
+//        assertTrue(blackjack.isHasSplit());
+//
+//    }
+//
+//    @Test
+//    void toggleTargetHand() {
+//        blackjack.startGame(10);
+//        blackjack.toggleTargetHand();
+//        assertEquals(blackjack.getTargetHand(), blackjack.getPlayersHand2());
+//        blackjack.toggleTargetHand();
+//        assertEquals(blackjack.getTargetHand(), blackjack.getPlayersHand1());
+//    }
+//
+//    @Test
+//    void dealerPlay() {
+//        blackjack.startGame(10);
+//        blackjack.dealerPlay();
+//        Assertions.assertTrue(blackjack.getDealersHand().getSumOfDeck() >= 17);
+//        Assertions.assertFalse(blackjack.getDealersHand().isActive());
+//        if (blackjack.getDealersHand().getSumOfDeck() == 21){
+//            assertEquals(0, blackjack.getPayout());
+//        }
+//        if (blackjack.getPlayersHand2().getSumOfDeck()!=0){
+//            assertTrue(blackjack.getPayout()-blackjack.getBet() != blackjack.getPayout());
+//        }
+//    }
+//
+//
+//    void gameSetup(){
+//        blackjack.setBet(10);
+//        blackjack.fieldSetup();
+//    }
+//
+//
+//    void dealerCardSetup(Card card1, Card card2){
+//        Hand hand = new Hand();
+//        hand.getDeck().addAll(Arrays.asList(card1,card2));
+//        blackjack.setDealersHand(hand);
+//    }
+//
+//    void playerCardSetup(Card card1, Card card2){
+//        Hand hand = new Hand();
+//        hand.getDeck().addAll(Arrays.asList(card1,card2));
+//        blackjack.setPlayersHand1(hand);
+//    }
+
+    private Card selectCard(String name){
+        return cardDeck.getDeck().stream().filter(
+                card -> card.getCardName().equals(name))
+                .collect(Collectors.toList()).get(0);
+    }
+
+
 }
