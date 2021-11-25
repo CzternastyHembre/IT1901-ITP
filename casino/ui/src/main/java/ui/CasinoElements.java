84a074724c104b7e9ac39874421214bf67c59be4
Sebastian Sole <sebastrs@stud.ntnu.no>
2021-11-25 13:58:59 +0100

    refactor: Renamed the CasinoElementsControlelr
    Renamed CasinoElements class and fields to have camel case convention
    
    Issue: #71

--- casino/ui/src/main/java/ui/CasinoElements.java
+++ casino/ui/src/main/java/ui/CasinoElements.java
@@ -15,34 +15,34 @@ import javafx.scene.text.FontWeight;
  * Creates standard circles and chips in the UI.
  */
 
-public class CASINO_ELEMENTS {
+public class CasinoElements {
 
-  private static final String[] COLORS = {
+  private static final String[] colors = {
     "#a60000", "#cfc132", "#1b32a8", "#ffffff", "#4a4a4a", "ed3434", "12ff34" };
-  private static final int[] VALUES = { 1, 5, 25, 50, 100, 500, 1000, 5000, 10000, 50000 };
-  private static final String[] TEXT_VALUES = {
+  private static final int[] values = { 1, 5, 25, 50, 100, 500, 1000, 5000, 10000, 50000 };
+  private static final String[] textValues = {
     "  1", "  5", " 25", " 50", "100", "500", " 1k", " 5k", "10k", "50k" };
-  public static final double CHIP_RADIUS = 20;
-  public static final double FONTSIZE = 15;
-  public static final double LARGE_FONTSIZE = 30;
-  public static final Font TEXT_FONT = Font.font("Arial", FontWeight.BOLD, FONTSIZE);
-  public static final Font LARGE_TEXT_FONT = Font.font("Arial", FontWeight.BOLD, LARGE_FONTSIZE);
-  public static final Color TEXT_COLOR = Color.WHITE;
+  public static final double chipRadius = 20;
+  public static final double fontSize = 15;
+  public static final double largeFontSize = 30;
+  public static final Font textFont = Font.font("Arial", FontWeight.BOLD, fontSize);
+  public static final Font largeTextFont = Font.font("Arial", FontWeight.BOLD, largeFontSize);
+  public static final Color textColor = Color.WHITE;
 
   public static String getColor(int index) {
-    return COLORS[index % COLORS.length];
+    return colors[index % colors.length];
   }
 
   public static int getValue(int index) {
-    return VALUES[index];
+    return values[index];
   }
 
   public static int getValuesSize() {
-    return VALUES.length;
+    return values.length;
   }
 
   public static String getTextValue(int index) {
-    return TEXT_VALUES[index];
+    return textValues[index];
   }
 
   /**
@@ -52,8 +52,8 @@ public class CASINO_ELEMENTS {
   public static Pane getChip(int valueIndex) {
 
     Circle circle = new Circle();
-    circle.setRadius(CHIP_RADIUS);
-    circle.setFill(Color.valueOf(CASINO_ELEMENTS.getColor(valueIndex)));
+    circle.setRadius(chipRadius);
+    circle.setFill(Color.valueOf(CasinoElements.getColor(valueIndex)));
     circle.setStroke(Paint.valueOf("black"));
     circle.setStrokeWidth(6);
     circle.setStrokeLineCap(StrokeLineCap.BUTT); // The poker chip border design
@@ -61,7 +61,7 @@ public class CASINO_ELEMENTS {
     circle.setStyle("-fx-stroke-dash-array:8;");
 
     Circle circle2 = new Circle(); // To get a "second border on the chip
-    circle2.setRadius(CHIP_RADIUS);
+    circle2.setRadius(chipRadius);
     circle2.setStroke(Paint.valueOf("black"));
 
     Pane chipContainer = new Pane();
@@ -69,10 +69,10 @@ public class CASINO_ELEMENTS {
     chipContainer.getChildren().add(circle2);
     chipContainer.getChildren().add(circle);
 
-    Label chipLabel = new Label(CASINO_ELEMENTS.getTextValue(valueIndex));
-    chipLabel.setFont(TEXT_FONT);
-    chipLabel.setTranslateX(-FONTSIZE / 3 * 2 - 3);
-    chipLabel.setTranslateY(-FONTSIZE / 2 - 2);
+    Label chipLabel = new Label(CasinoElements.getTextValue(valueIndex));
+    chipLabel.setFont(textFont);
+    chipLabel.setTranslateX(-fontSize / 3 * 2 - 3);
+    chipLabel.setTranslateY(-fontSize / 2 - 2);
     chipLabel.setTextFill(Paint.valueOf("black"));
 
     chipContainer.getChildren().add(chipLabel);
