9ba6cc14d091f323d1b9737afedbc8f21030dcbc
Sebastian Sole <sebastrs@stud.ntnu.no>
2021-11-25 17:45:37 +0100

    style: Cleaned up UI code
    Removed unused imports and comments from UI
    
    Issue: #77

--- casino/ui/src/main/java/ui/App.java
+++ casino/ui/src/main/java/ui/App.java
@@ -3,7 +3,6 @@ package ui;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
