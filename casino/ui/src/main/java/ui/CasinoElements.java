fcfa4123965cd78b06bc6824bb34bab9e85ef3ff
Sebastian Sole <sebastrs@stud.ntnu.no>
2021-11-25 14:53:59 +0100

    refactor: Removed unused field
    Removed unused field
    
    Issue: #71

--- casino/ui/src/main/java/ui/CasinoElements.java
+++ casino/ui/src/main/java/ui/CasinoElements.java
@@ -19,9 +19,7 @@ import java.util.stream.Collectors;
  */
 
 public class CasinoElements {
-
-  private static final String[] colors = {
-    "#a60000", "#cfc132", "#1b32a8", "#ffffff", "#4a4a4a", "ed3434", "12ff34" };
+  
   private static final int[] values = { 1, 5, 25, 50, 100, 500, 1000, 5000, 10000, 50000 };
   private static final String[] textValues = {
     "  1", "  5", " 25", " 50", "100", "500", " 1k", " 5k", "10k", "50k" };
